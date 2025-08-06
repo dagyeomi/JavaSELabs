e66469861079523f3fa0f3aa7e4c6b8487b252b2
dagyeomi <kyunghwa011@gmail.com>
2025-08-06 13:45:56 +0900

    account getter setter

--- src/workshop/account/entity/account.java
+++ src/workshop/account/entity/account.java
@@ -1,5 +1,35 @@
 package workshop.account.entity;
 
 public class account {
+	private String custID;
+	private String acctID;
+	private int balance;
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+		
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
 
+	public String getCustID() {
+		return custID;
+	}
+
+	public void setCustID(String custID) {
+		this.custID = custID;
+	}
+
+	
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+	
+	public String getAcctId() {
+		return acctId;
+	}
+	
+	
 }
