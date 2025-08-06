c20d15ea28bc1d7291547744636b1ec5c3cc321d
dagyeomi <kyunghwa011@gmail.com>
2025-08-06 14:52:21 +0900

    출금립금

--- src/workshop/account/entity/account.java
+++ src/workshop/account/entity/account.java
@@ -1,43 +1,55 @@
 package workshop.account.entity;
 
-public class account {
-	private String custID;
-	private String acctID;
+public class Account {
+	private String custId;
+	private String acctId;
 	private int balance;
-	
-	public void setBalance(int balance) {
-		this.balance = balance;
-		
+
+	// default constructor 선언
+	public Account() {
+		System.out.println("기본생성자 호출됨");
 	}
-	
+
+	// Constructor Overloading (생성자 중복정의)
+	public Account(String custId, String acctId, int balance) {
+		//this.custId = custId;
+		//this.acctId = acctId;
+		setCustId(custId);
+		setAcctId(acctId);
+		this.balance = balance;
+	}
+
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	}
+
 	public int getBalance() {
 		return balance;
 	}
 
-	public String getCustID() {
-		return custID;
+	public String getCustId() {
+		return custId;
 	}
 
-	public void setCustID(String custID) {
-		this.custID = custID;
+	public void setCustId(String custId) {
+		this.custId = custId;
 	}
 
-	
 	public void setAcctId(String acctId) {
 		this.acctId = acctId;
 	}
-	
+
 	public String getAcctId() {
 		return acctId;
 	}
-	
-	//입금
+
+	// 입금
 	public void deposit(int amount) {
 		this.balance += amount;
 	}
-	//출금
+
+	// 출금
 	public void withdraw(int amount) {
 		this.balance -= amount;
 	}
-	
 }
